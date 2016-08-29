ffc27b292e4267e937daf36b93f4c2115b10723f
Shiv arora <aroras@cromwell.co.uk>
2016-08-29 22:58:48 +0100

    make it better

---
+++ app/src/main/java/com/pdf/anu/pdf/YourPdfViewerActivity.java
@@ -0,0 +1,29 @@
+package com.pdf.anu.pdf;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by aroras on 28/08/2016.
+ */
+
+public class YourPdfViewerActivity extends  net.sf.andpdf.pdfviewer.PdfViewerActivity {
+    public int getPreviousPageImageResource() { return R.drawable.left_arrow; }
+    public int getNextPageImageResource() { return R.drawable.right_arrow; }
+    public int getZoomInImageResource() { return R.drawable.zoom_in; }
+    public int getZoomOutImageResource() { return R.drawable.zoom_out; }
+    public int getPdfPasswordLayoutResource() { return R.layout.pdf_file_password; }
+    public int getPdfPageNumberResource() { return R.layout.dialog_pagenumber; }
+    public int getPdfPasswordEditField() { return R.id.etPassword; }
+    public int getPdfPasswordOkButton() { return R.id.btOK; }
+    public int getPdfPasswordExitButton() { return R.id.btExit; }
+    public int getPdfPageNumberEditField() { return R.id.pagenum_edit; }
+
+}
